66820175776fff3cf79e654daa4d76065cbfb5c4
Chaudoreille <a.chaudoreille@gmail.com>
2023-02-16 17:33:50 +0100

    [IT 1] - add get + post + patch routes

--- routes/characters.route.js
+++ routes/characters.route.js
@@ -1,5 +1,6 @@
-const router = require('express').Router()
-const Character = require('../models/Character.model')
+const router = require("express").Router();
+const { default: mongoose } = require("mongoose");
+const Character = require("../models/Character.model");
 /**
  * !All the routes here are prefixed with /api/characters
  */
@@ -7,39 +8,123 @@ const Character = require('../models/Character.model')
 /**
  * ? This route should respond with all the characters
  */
-router.get('/', (req, res, next) => {
-	/**Your code goes here */
-})
+router.get("/", async (req, res, next) => {
+    try {
+        const allCharacters = await Character.find();
+
+        if (!allCharacters || !allCharacters.length) {
+            res.status(204);
+        } else {
+            res.status(200);
+        }
+        res.send(allCharacters);
+    } catch (error) {
+        next(error);
+    }
+});
 
 /**
  * ? This route should create one character and respond with
  * ? the created character
  */
-router.post('/', (req, res, next) => {
-	/**Your code goes here */
-})
+router.post("/", async (req, res, next) => {
+    try {
+        const errors = {};
+        const { name, occupation, weapon, cartoon } = req.body;
+
+        if (typeof name != "string") errors["name"] = name;
+        if (typeof occupation != "string") errors["occupation"] = occupation;
+        if (typeof weapon != "string") errors["weapon"] = weapon;
+        if (typeof cartoon != "boolean") errors["cartoon"] = cartoon;
+
+        if (Object.keys(errors).length > 0) {
+            return res.status(400).send(errors);
+        }
+
+        const characterCreation = await Character.create({
+            name,
+            occupation,
+            weapon,
+            cartoon,
+        });
+
+        res.status(201).send(characterCreation);
+    } catch (error) {
+        next(error);
+    }
+});
 
 /**
  * ? This route should respond with one character
  */
-router.get('/:id', (req, res, next) => {
-	/**Your code goes here */
-})
+router.get("/:id", async (req, res, next) => {
+    try {
+        const characterId = req.params.id;
+        if (!mongoose.isValidObjectId(characterId)) {
+            res.status(400).send("Invalid Character id");
+        }
+        const character = await Character.findById(characterId);
+
+        if (!character) {
+            res.status(204);
+        } else {
+            res.status(200);
+        }
+        res.send(character);
+    } catch (error) {
+        next(error);
+    }
+});
 
 /**
  * ? This route should update a character and respond with
  * ? the updated character
  */
-router.patch('/:id', (req, res, next) => {
-	/**Your code goes here */
-})
+router.patch("/:id", async (req, res, next) => {
+    try {
+        const errors = {};
+        const { name, occupation, weapon, cartoon } = req.body;
+
+        const characterId = req.params.id;
+
+        if (!mongoose.isValidObjectId(characterId)) {
+            errors.id = "Invalid Character id";
+        }
+        if (name && typeof name != "string") errors["name"] = name;
+        if (occupation && typeof occupation != "string")
+            errors["occupation"] = occupation;
+        if (weapon && typeof weapon != "string") errors["weapon"] = weapon;
+        if (cartoon && typeof cartoon != "boolean") errors["cartoon"] = cartoon;
+
+        if (Object.keys(errors).length > 0) {
+            return res.status(400).send(errors);
+        }
+
+        const character = await Character.findByIdAndUpdate(
+            characterId,
+            {
+                name,
+                occupation,
+                weapon,
+                cartoon,
+            },
+            {
+                new: true,
+            }
+        );
+
+        res.status(200).send(character);
+    } catch (error) {
+        next(error);
+    }
+});
 
 /**
  * ? Should delete a character and respond with a success or
  * ? error message
  */
-router.delete('/:id', (req, res, next) => {
-	/**Your code goes here */
-})
+router.delete("/:id", (req, res, next) => {
+    /**Your code goes here */
+});
 
-module.exports = router
+module.exports = router;

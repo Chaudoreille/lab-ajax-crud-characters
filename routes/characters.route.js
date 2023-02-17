81a9750eefdd85ed8fdaa02fd44b0f430da7b289
Chaudoreille <a.chaudoreille@gmail.com>
2023-02-17 11:20:47 +0100

    [HIPSTER] - add validator middlewares before it was cool

--- routes/characters.route.js
+++ routes/characters.route.js
@@ -5,6 +5,36 @@ const Character = require("../models/Character.model");
  * !All the routes here are prefixed with /api/characters
  */
 
+const paramIdValidator = (req, res, next) => {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        return res.status(400).send("Invalid Character id");
+    }
+    next();
+};
+
+const bodyCharacterValidator = (req, res, next) => {
+    const errors = {};
+    const { name, occupation, weapon, cartoon } = req.body;
+
+    if (name && typeof name != "string") {
+        errors["name"] = name;
+    }
+    if (occupation && typeof occupation != "string") {
+        errors["occupation"] = occupation;
+    }
+    if (weapon && typeof weapon != "string") {
+        errors["weapon"] = weapon;
+    }
+    if (cartoon && typeof cartoon != "boolean") {
+        errors["cartoon"] = cartoon;
+    }
+
+    if (Object.keys(errors).length > 0) {
+        return res.status(400).send(errors);
+    }
+    next();
+};
+
 /**
  * ? This route should respond with all the characters
  */
@@ -12,12 +42,9 @@ router.get("/", async (req, res, next) => {
     try {
         const allCharacters = await Character.find();
 
-        if (!allCharacters || !allCharacters.length) {
-            res.status(204);
-        } else {
-            res.status(200);
-        }
-        res.send(allCharacters);
+        res.status(allCharacters && allCharacters.length ? 200 : 204).send(
+            allCharacters
+        );
     } catch (error) {
         next(error);
     }
@@ -27,26 +54,9 @@ router.get("/", async (req, res, next) => {
  * ? This route should create one character and respond with
  * ? the created character
  */
-router.post("/", async (req, res, next) => {
+router.post("/", bodyCharacterValidator, async (req, res, next) => {
     try {
-        const errors = {};
-        const { name, occupation, weapon, cartoon } = req.body;
-
-        if (typeof name != "string") errors["name"] = name;
-        if (typeof occupation != "string") errors["occupation"] = occupation;
-        if (typeof weapon != "string") errors["weapon"] = weapon;
-        if (typeof cartoon != "boolean") errors["cartoon"] = cartoon;
-
-        if (Object.keys(errors).length > 0) {
-            return res.status(400).send(errors);
-        }
-
-        const characterCreation = await Character.create({
-            name,
-            occupation,
-            weapon,
-            cartoon,
-        });
+        const characterCreation = await Character.create(req.body);
 
         res.status(201).send(characterCreation);
     } catch (error) {
@@ -57,20 +67,11 @@ router.post("/", async (req, res, next) => {
 /**
  * ? This route should respond with one character
  */
-router.get("/:id", async (req, res, next) => {
+router.get("/:id", paramIdValidator, async (req, res, next) => {
     try {
-        const characterId = req.params.id;
-        if (!mongoose.isValidObjectId(characterId)) {
-            res.status(400).send("Invalid Character id");
-        }
-        const character = await Character.findById(characterId);
+        const character = await Character.findById(req.params.id);
 
-        if (!character) {
-            res.status(204);
-        } else {
-            res.status(200);
-        }
-        res.send(character);
+        res.status(character ? 200 : 204).send(character);
     } catch (error) {
         next(error);
     }
@@ -80,60 +81,41 @@ router.get("/:id", async (req, res, next) => {
  * ? This route should update a character and respond with
  * ? the updated character
  */
-router.patch("/:id", async (req, res, next) => {
-    try {
-        const errors = {};
-        const { name, occupation, weapon, cartoon } = req.body;
+router.patch(
+    "/:id",
+    paramIdValidator,
+    bodyCharacterValidator,
+    async (req, res, next) => {
+        try {
+            const character = await Character.findByIdAndUpdate(
+                req.params.id,
+                req.body,
+                {
+                    new: true,
+                }
+            );
 
-        const characterId = req.params.id;
-
-        if (!mongoose.isValidObjectId(characterId)) {
-            errors.id = "Invalid Character id";
+            res.status(200).send(character);
+        } catch (error) {
+            next(error);
         }
-        if (name && typeof name != "string") errors["name"] = name;
-        if (occupation && typeof occupation != "string")
-            errors["occupation"] = occupation;
-        if (weapon && typeof weapon != "string") errors["weapon"] = weapon;
-        if (cartoon && typeof cartoon != "boolean") errors["cartoon"] = cartoon;
-
-        if (Object.keys(errors).length > 0) {
-            return res.status(400).send(errors);
-        }
-
-        const character = await Character.findByIdAndUpdate(
-            characterId,
-            {
-                name,
-                occupation,
-                weapon,
-                cartoon,
-            },
-            {
-                new: true,
-            }
-        );
-
-        res.status(200).send(character);
-    } catch (error) {
-        next(error);
     }
-});
+);
 
 /**
  * ? Should delete a character and respond with a success or
  * ? error message
  */
-router.delete("/:id", async (req, res, next) => {
+router.delete("/:id", paramIdValidator, async (req, res, next) => {
     try {
-        const characterId = req.params.id;
+        const deletedCharacter = await Character.findByIdAndDelete(
+            req.params.id
+        );
 
-        if (!mongoose.isValidObjectId(characterId)) {
+        if (!deletedCharacter) {
             return res.status(400).send("Invalid Character id");
         }
-
-        const character = await Character.findByIdAndDelete(characterId);
-
-        res.status(204).send();
+        res.status(204).send(deletedCharacter);
     } catch (error) {
         next(error);
     }

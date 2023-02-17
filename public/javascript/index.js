9fc3599e65a9434e629ad0a66a26f0cb7f44e83e
Chaudoreille <a.chaudoreille@gmail.com>
2023-02-17 14:23:22 +0100

    [IT 2] - WIP - completed fetch-all, fetch-one and delete methods

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,20 +2,40 @@
  * You might want to use this template to display each new characters
  * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template#examples
  */
-const characterTemplate = document.getElementById("template");
+const characterTemplate = document.querySelector("#template").content;
 const characterContainer = document.querySelector(".characters-container");
 const apiUrl = "api/characters";
+const fetchOneIdInput = document.querySelector("input[name=character-name]");
+const deleteIdInput = document.querySelector("input[name=character-id-delete]");
+
+function fillCharacter(domCharacterInfo, data) {
+    const charInfo = domCharacterInfo.querySelector(".character-info");
+    const id = domCharacterInfo.querySelector(".character-id > span");
+    const name = domCharacterInfo.querySelector(".name > span");
+    const occupation = domCharacterInfo.querySelector(".occupation > span");
+    const cartoon = domCharacterInfo.querySelector(".cartoon > span");
+    const weapon = domCharacterInfo.querySelector(".weapon > span");
+
+    charInfo.dataset.id = data._id;
+    id.innerText = data._id;
+    name.innerText = data.name;
+    occupation.innerText = data.occupation;
+    cartoon.innerText = data.cartoon ? "TRUE" : "FALSE";
+    weapon.innerText = data.weapon;
+}
 
 document
     .getElementById("fetch-all")
     .addEventListener("click", async function (event) {
         try {
             const response = await axios.get(`${apiUrl}`);
+
             if (response.status === 200) {
                 characterContainer.innerHTML = "";
                 response.data.forEach((character) => {
-                    const newCharacter =
-                        characterTemplate.firstChild.cloneNode();
+                    const newCharacter = characterTemplate.cloneNode(true);
+                    fillCharacter(newCharacter, character);
+                    characterContainer.append(newCharacter);
                 });
             }
         } catch (error) {
@@ -25,11 +45,42 @@ document
 
 document
     .getElementById("fetch-one")
-    .addEventListener("click", function (event) {});
+    .addEventListener("click", async function (event) {
+        try {
+            const id = fetchOneIdInput.value;
+            const response = await axios.get(`${apiUrl}/${id}`);
+
+            if (response.status === 200) {
+                const newCharacter = characterTemplate.cloneNode(true);
+                characterContainer.innerHTML = "";
+                fillCharacter(newCharacter, response.data);
+                characterContainer.append(newCharacter);
+                fetchOneIdInput.value = "";
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    });
 
 document
     .getElementById("delete-one")
-    .addEventListener("click", function (event) {});
+    .addEventListener("click", async function (event) {
+        try {
+            const id = deleteIdInput.value;
+            const response = await axios.delete(`${apiUrl}/${id}`);
+
+            if (response.status === 204) {
+                document
+                    .querySelectorAll(`.character-info[data-id="${id}"]`)
+                    .forEach((deletedCharacter) => {
+                        deletedCharacter.remove();
+                    });
+                deleteIdInput.value = "";
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    });
 
 document
     .getElementById("edit-character-form")
